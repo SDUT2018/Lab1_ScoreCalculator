821e8f421722a955b9730faf04b272d40d1f1c71
David Fisher <fisherds@rose-hulman.edu>
2018-05-28 14:51:56 +0800

    progress so far

--- ScoreCalculator/app/src/main/java/edu/rosehulman/fisherds/scorecalculator/MainActivity.java
+++ ScoreCalculator/app/src/main/java/edu/rosehulman/fisherds/scorecalculator/MainActivity.java
@@ -9,5 +9,9 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    // TODO for you....
+    // In code (here) change the color_points_textview to say "42 color points"
+
   }
 }
